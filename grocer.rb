81e7b23b88f27cd4ad917dc4f8de2489b2f20ad2
Bri Turner <blturner3527@gmail.com>
2020-01-14 00:26:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,9 +25,6 @@ def consolidate_cart(cart)
       end
       inner_index+= 1 
     end
-    if new_cart[0][:item] == cart[index][:item]
-    new_cart[index] = cart[index]
-    new_cart[index][:count] = 1
        
     index += 1 
   end
