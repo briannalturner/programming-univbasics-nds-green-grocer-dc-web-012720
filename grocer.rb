3d4b0cd411b199d9e12f175533af6e7b360c9940
Bri Turner <blturner3527@gmail.com>
2020-01-14 01:56:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,7 @@ def apply_coupons(cart, coupons)
     if cart_item && (cart_item[:count] >= coupons[index][:num])
       if cart_item_with_coupon
         cart_item_with_coupon[:count] += coupons[index][:num]
-        cart_item[:count] -= 
+        cart_item[:count] -= coupons[index][:num]
     end
         
         
