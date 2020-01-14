28fee6e5b7a0908eeb104cdd6bd7895cad896e9c
Bri Turner <blturner3527@gmail.com>
2020-01-14 00:07:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,13 +10,6 @@ def find_item_by_name_in_collection(name, collection)
   end
 end
 
-items = [
-        { :item => "DOG FOOD" },
-        { :item => "WINE" },
-        { :item => "STRYCHNINE" }
-      ]
-puts find_item_by_name_in_collection("Jelly", items)
-
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
