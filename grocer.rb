f3f19114d6736f56e795a88254ff665f99a9e5e6
Bri Turner <blturner3527@gmail.com>
2020-01-14 02:17:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def find_item_by_name_in_collection(name, collection)
   index = 0 
   
-  while index < collection.length do 
+  while index < collection.length 
     if name != collection[index][:item]
       index +=1
     else
