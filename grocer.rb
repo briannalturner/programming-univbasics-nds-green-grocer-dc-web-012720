2c948c79675a2c3334f5558786e65efd3ee282ae
Bri Turner <blturner3527@gmail.com>
2020-01-14 01:56:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,7 +51,10 @@ def apply_coupons(cart, coupons)
     couponed_item = "#{coupons[index][:item]} W/ COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item, cart)
     if cart_item && (cart_item[:count] >= coupons[index][:num])
-      
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[index][:num]
+        cart_item[:count] -= 
+    end
         
         
     index += 1 
