5660eb3f27fd2904c8cf6d4224e1a425e3967139
Bri Turner <blturner3527@gmail.com>
2020-01-14 00:24:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
     #iterate over new cart to see if anything matches
     while inner_index < new_cart.length do
       if new_cart[inner_index][:item] == cart[index][:item]
-        new_cart[index][:count] += 1 
+        new_cart[inner_index][:count] += 1 
       end
       
       inner_index+= 1 
