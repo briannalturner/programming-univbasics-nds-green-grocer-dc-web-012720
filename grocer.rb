9bbb58a4245f2dc871530be8d7cfb6f8774440ca
Bri Turner <blturner3527@gmail.com>
2020-01-14 02:07:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,7 +61,7 @@ def apply_coupons(cart, coupons)
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon 
-        cart_item[:count] -+ coupons[:count]
+        cart_item[:count] -= coupons[:count]
       end
     end
         
